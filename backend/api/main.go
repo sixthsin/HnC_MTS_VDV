72916438f7edc8fdc2c18a923f950739f7998e4e
Vadim Moiseev <sixtysixthone@yandex.ru>
2024-11-29 21:16:35 +0300

    Added base for servers

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"first_name"`
+	Email string `json:"email"`
+}
+
+func MainHAndler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("Product %s", id)
+	fmt.Fprint(w, response)
+
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	r := mux.NewRouter()
+
+	// Маршруты
+	r.HandleFunc("/products/{id:[0-9]+}", MainHAndler)
+	http.Handle("/", r)
+	log.Println("Start listening on port :8000")
+	http.ListenAndServe(":8000", r)
 }
--- backend/py_data/data_sec.py
+++ backend/py_data/data_sec.py
@@ -1,3 +1,4 @@
 a = 1
 while a > 0:
     print('hello world')  # ada
+    
